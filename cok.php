df9bad53ecdfff365898d2b61712fb0e46cbec08
jalang <35096599+jalang11@users.noreply.github.com>
2018-12-19 12:38:10 +0700

    Create cok.php

---
+++ cok.php
@@ -0,0 +1,17 @@
+<?php
+include 'tes.php';
+/*
+    https://github.com/jalang11/sms
+    Made by OMTampanz
+*/
+$init = new Bom();
+//Eksekusi Call/Sms Boomber (Limit 3x/Jam)
+$init->type = 2; //Type 2 untuk telpon, Type 1 untuk sms
+$init->no = ""; //Nomer Hp tujuan
+if ($init->type == 1) {
+	for ($i=0; $i < 2; $i++) { 
+	    $init->Verif($init->no,$init->type);
+	}
+}elseif ($init->type == 2) {
+	 $init->Verif($init->no,$init->type);
+}
